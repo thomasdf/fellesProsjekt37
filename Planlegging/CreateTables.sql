6d844ab12ac1110d2ee44a63dbde421ff4da73cf
thomasdf <thomasdf@stud.ntnu.no>
2015-02-25 16:12:50 +0100

    end of day

--- Planlegging/CreateTables.sql
+++ Planlegging/CreateTables.sql
@@ -3,24 +3,35 @@ drop schema fellesprosjekt;
 create schema fellesprosjekt;
 use fellesprosjekt;
 
+create table person( #entity. This is an entity to be created before creating an account for the person
+employee_nr int(5) unique not null primary key,
+first_name varchar(10) not null,
+last_name varchar(10) not null,
+mobile_nr char(8) not null
+);
+
+create table calendar(
+calendar_id int not null unique primary key,
+user_name varchar(10) unique not null,
+calendarGroup_id int unique not null,
+activity_id int(5) unique
+);
+
+Create table calendarGroup( #entity, including groupHasCalendar-relation
+group_id int unique not null primary key,
+group_name varchar(10),
+calendar_id int not null unique,
+foreign key(calendar_id) references calendar(calendar_id)
+);
+
 Create table account( #entity including hasCalendar and hasAccount relation
 user_name varChar(10) unique primary key,
 employee_nr int(5) unique not null,
 /*not null to assure that an account cannot exist without a relation to a person-entity.
 unique to ensure that there is 1 and only 1 person corresponding to 1 and only 1 account.*/
-activity_id int,
-room_name varchar(10),
-group_id int,
 calendar_id int not null,
 foreign key(employee_nr) references person(employee_nr) on delete cascade, #hasAccount
 foreign key(calendar_id) references calendar(calendar_id) on delete cascade #hasCalendar
-);
-
-Create table calendarGroup( #entity, including groupHasCalendar-relation
-group_id int unique primary key,
-group_name varchar(10),
-calendar_id int not null unique,
-foreign key(calendar_id) references calendar(calendar_id)
 );
 
 create table subGroup( #subgroup-Relation
@@ -30,6 +41,11 @@ supergroup_id int,
 constraint unique (subgroup_id, supergroup_id),
 foreign key(subgroup_id) references calendarGroup(group_id),
 foreign key(supergroup_id) references calendarGroup(group_id)
+);
+
+Create table room( #entity including isAdmin-relationship
+room_name varchar(10) unique primary key,
+capacity varchar(3)
 );
 
 create table isMember( #isMember-relation
@@ -43,22 +59,8 @@ foreign key(user_name) references account(user_name) on delete cascade
 #delete on cascade is used to assure that the relation is deleted if either the corresponding account or group is deleted
 );
 
-Create table room( #entity including isAdmin-relationship
-room_name varchar(10) unique primary key,
-admin_user_name varchar(10),
-foreign key(admin_user_name) references account(user_name) on delete cascade
-#the relationship isAdmin between accound and room. Delete on cascade to assure that the relation is deletes if its corresponding account is deleted.
-);
-
-create table person( #entity. This is an entity to be created before creating an account for the person
-employee_nr int(5) unique not null primary key,
-first_name varchar(10),
-last_name varchar(10),
-mobile_nr char(8)
-);
-
 create table acitivity( #entity. includes isOwner-relation.
-activity_id int unique primary key,
+activity_id int(5) unique primary key,
 description varchar(256),
 activity_date date,
 start_time time,
@@ -66,21 +68,25 @@ end_time time,
 repetition int,
 end_date date,
 owner_user_name varchar(10) not null, # ensures 1 owner
-foreign key(owner_user_name) references account(user_name) #owner is a reserved word, and owner_user_name is used instead.
+room_name varchar(10),
+foreign key(owner_user_name) references account(user_name), #owner is a reserved word, and owner_user_name is used instead.
+foreign key(room_name) references room(room_name)
 );
 
 create table invited( #relation n-n between account and activity
-activity_id int,
-user_name varchar(10),
-primary key (activity_id, user_name), #to ensure that no duplicated invitations to one activity exists.
-foreign key(activity_id) references activity(activity_id) on delete cascade,
-foreign key(user_name) references account(user_name) on delete cascade
+activity_id int(5),
+user_name varchar(10)
 );
 
-create table calendar( #including hasCalendar
-calendar_id int not null unique primary key,
-user_name varchar(10) unique,
-calendarGroup_id int unique,
-foreign key (calendarGroup_id) references calendarGroup(group_id),
-foreign key(user_name) references account(user_name)
-);
+
+#altering tables for foreign keys
+
+alter table calendar
+add foreign key (calendarGroup_id) references calendarGroup(group_id),
+add foreign key(user_name) references account(user_name),
+add foreign key(activity_id) references activity(activity_id);
+
+alter table invited#problems underneath
+#add constraint unique (activity_id, user_name),
+#add foreign key(activity_id) references activity(activity_id) on delete cascade,
+add foreign key(user_name) references account(user_name) on delete cascade;
