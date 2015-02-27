9372a69613d534b1df38b6a34c2475c966bb8889
thomasdf <thomasdf@stud.ntnu.no>
2015-02-27 13:49:14 +0100

    lagt til info om presedens

--- Planlegging/DBtestData.sql
+++ Planlegging/DBtestData.sql
@@ -1,8 +1,15 @@
 
 /*
-rekkefølgen for innsetting er viktig her. Først person, så calendar, så account
+rekkefølgen for innsetting er viktig her. Entitetene først. Følgende rekkefølge gjelder:
+Følgende presedens for entiteter vil alltid virke. Andre rekkefølger kan virke, men trenger ikke virke. Derfor: det er ofte fornuftig å opprette ting i denne rekkefølgen, også i fremtiden.
+-Person, Account, Calendar, CalendarGroup
+og følgende entiter kan opprettes helt uavhengig av andre entiteter:
+- room, 
+
+for relasjonene gjelder det at begge entitetene vi skal relatere må eksistere før relasjonen kan opprettes.
+
 */
-/*
+
 insert into person
 values 
 (12345, 'Spooderman', 'Kent', '81549300'),
@@ -15,7 +22,19 @@ values
 (87931, 'Howling mad', 'Murloc', '13377331'),
 (99883, 'Jim', 'Lahey', '13371337'),
 (13371, 'J-', 'Rock', '77337733');
-*/
+
+insert into account
+values
+('Spidey', 12345, 11111),
+('manBats', 67890, 22222),
+('S.Meister', 24680, 33333),
+('McLovin', 13579, 44444),
+('Hannibal', 13806, 55555),
+('Faceman', 76487, 66666),
+('Baracus', 75894, 77777),
+('Murloc', 87931, 88888),
+('Lahey', 99883, 99999),
+('J-Rock', 13371, 00000);
 
 insert into calendar
 values
@@ -31,17 +50,3 @@ values
 (00000, 'J-Rock', null, null);
 
 
-/*
-insert into account
-values
-('Spidey', 12345, 11111),
-('manBats', 67890, 22222),
-('S.Meister', 24680, 33333),
-('McLovin', 13579, 44444),
-('Hannibal', 13806, 55555),
-('Faceman', 76487, 66666),
-('Baracus', 75894, 77777),
-('Murloc', 87931, 88888),
-('Lahey', 99883, 99999),
-('J-Rock', 13371, 00000);
-*/
