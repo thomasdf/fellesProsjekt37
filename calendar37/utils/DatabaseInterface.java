36bcca12ae3d8a960b206dd0d464069f77e3db0a
thomasdf <thomasdf@stud.ntnu.no>
2015-03-09 15:49:07 +0100

    lagt til getters for calendarID, employeenr, fullname, mobile og password

--- calendar37/utils/DatabaseInterface.java
+++ calendar37/utils/DatabaseInterface.java
@@ -4,7 +4,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  * 
  * DatabaseInterface provides a connection to the database. This is based on the
@@ -124,14 +123,106 @@ public class DatabaseInterface	{
 		}
 	}
 	
-	public int getEmployeeNr(String user_name) throws SQLException {
+	/**
+	 * Returns the employee-number registered in an account.
+	 * @param user_name the user name which is the primary key of the account we want to find the employee number from.
+	 * @return the employee-number for the account.
+	 */
+	public int getEmployeeNr(String user_name) {
+		int employeeNr = 0;
 		try	{
 			this.resultset = this.statement.executeQuery("select person.employee_nr from person, account where account.employee_nr = person.employee_nr and account.user_name = " +"\"" + user_name +"\"");
+			resultset.next();
+			employeeNr = this.resultset.getInt(1);
 		}	catch (SQLException e)	{
 			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
 		}
-		return resultset.getInt(0);
+		return employeeNr;
 	}
 	
+	/**
+	 * Returns the full name registered in an account.
+	 * @param user_name the user name which is the primary key of the account we want to find the full name from.
+	 * @return the full name for the account.
+	 */
+	public String getFullName(String user_name){
+		String fullName = null;
+		try{
+			this.resultset = this.statement.executeQuery("select person.first_name, person.last_name from person, account where account.employee_nr = person.employee_nr and account.user_name = " +"\"" + user_name + "\"");
+			resultset.next();
+			fullName = this.resultset.getString(1) + " " + this.resultset.getString(2);
+		}	catch (SQLException e)	{
+			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
+		}
+		return fullName;
+	}
+	
+	/**
+	 * Returns a mobile number registered in an account in the database.
+	 * @param user_name the user name which is the primary key of the account we want to find the mobile number from.
+	 * @return the mobile number for the account
+	 */
+	public String getMobile(String user_name){
+		String mobile = null;
+		try{
+			this.resultset = this.statement.executeQuery("select person.mobile_nr from person, account where account.employee_nr = person.employee_nr and account.user_name = " +"\"" + user_name + "\"");
+			resultset.next();
+			mobile = this.resultset.getString(1);
+		}	catch (SQLException e)	{
+			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
+		}
+		return mobile;
+	}
+	
+	/**
+	 * Returns a password registered in an account in the database.
+	 * @param user_name the user name which is the primary key of the account we want to find the password from.
+	 * @return the password for the account.
+	 */
+	public String getPassword(String user_name){ //this method is not a smart way to do things
+		String password = null;
+		try{
+			this.resultset = this.statement.executeQuery("select account.user_password from account where account.user_name = " +"\"" + user_name + "\"");
+			resultset.next();
+			password = this.resultset.getString(1);
+		}	catch (SQLException e)	{
+			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
+		}
+		return password;
+	}
+	
+	/**
+	 * Returns an ID for the calendar related to an account
+	 * @param user_name the user name which is the primary key of the account we want to find the calendarID from.
+	 * @return the calendardId for the account.
+	 */
+	public int getCalendarId(String user_name) {
+		int calendarId = 0;
+		try	{
+			this.resultset = this.statement.executeQuery("select calendar.calendar_id from account, calendar, hascalendar where hascalendar.user_name = account.user_name and hascalendar.calendar_id = calendar.calendar_id and hascalendar.user_name = " +"\"" + user_name +"\"");
+			resultset.next();
+			calendarId = this.resultset.getInt(1);
+		}	catch (SQLException e)	{
+			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
+		}
+		return calendarId;
+	}
+	
+	/**
+	 * Returns an ID for the calendar related to a group
+	 * @param groupId the groupID which is the primary key for the group we want to find the calendar from.
+	 * @return a calendarId for the groupCalendar.
+	 */
+	public int getCalendarId(int groupId) {
+		int calendarId = 0;
+		try	{
+			this.resultset = this.statement.executeQuery("select calendar.calendar_id from calendargroup, calendar, grouphascalendar where calendar.calendar_id = grouphascalendar.calendar_id and calendargroup.group_id = grouphascalendar.group_id and calendarGroup.group_id = " + groupId);
+			resultset.next();
+			calendarId = this.resultset.getInt(1);
+		}	catch (SQLException e)	{
+			System.out.println("Error from DatabaseInterface: " + e.getLocalizedMessage());
+		}
+		return calendarId;
+	}
 	
 }
