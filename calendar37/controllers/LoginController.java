2eb47858413cdf796675f22de3f34fc6fc42363c
thomasdf <thomasdf@stud.ntnu.no>
2015-03-12 15:03:35 +0100

    nullpointerexception

--- calendar37/controllers/LoginController.java
+++ calendar37/controllers/LoginController.java
@@ -29,10 +29,11 @@ public class LoginController implements Initializable{
 	public void login(ActionEvent login_event){
 		DatabaseInterface databaseinterface = new DatabaseInterface();
 		String user_name = txt_user_name.getText();
+		String db_password = databaseinterface.getPassword(user_name);
 		
 		if(!databaseinterface.isUsername(user_name)){//user_name does not exist.
 			label_error.setText("Brukernavnet eksisterer ikke. Sjekk at det er skrevet riktig.");
-		} else if(databaseinterface.getPassword(user_name).equals(txt_password.getText())){
+		} else if(db_password.equals(txt_password.getText())){
 				//login success-action!
 				label_error.setText("login success! horray!");
 			} else{
