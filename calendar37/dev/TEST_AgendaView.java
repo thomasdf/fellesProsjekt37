03756e828f78c53834627783cac094bb0c588966
Dag Erik Løvgren <dagerikhl@gmail.com>
2015-03-18 22:58:53 +0100

    Slettet og flyttet ting som ikke er til bruk i appen

--- calendar37/dev/TEST_AgendaView.java
+++ calendar37/dev/TEST_AgendaView.java
@@ -1,4 +1,5 @@
-package views;
+package dev;
+
 
 
 import javafx.application.Application;
@@ -7,14 +8,18 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
-public class DialogueView extends Application {
+public class TEST_AgendaView extends Application {
 
-	String viewName = "Dialogue";
+//	String viewName = "Calendar";
+//	String viewName = "Agenda";
+//	String viewName = "Dialogue";
+	String viewName = "Invite";
+//	String viewName = "Activity";
 	
 	@Override public void start(Stage primaryStage) throws Exception{
 		
 		//Sets the root
-		Parent root = FXMLLoader.load(getClass().getResource("/views/" + viewName + "View.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/views/CreateActivity.fxml"));
 		Scene scene = new Scene(root);
 		
 		//Adds a css-stylesheet to the scene
